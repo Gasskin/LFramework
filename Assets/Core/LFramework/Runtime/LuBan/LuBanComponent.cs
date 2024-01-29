b05b1745538751b51356281377c54985a0660d28
Gasskin <415753928@qq.com>
2024-01-29 18:46:08 +0800

    1

---
+++ Assets/Core/LFramework/Runtime/LuBan/LuBanComponent.cs
@@ -0,0 +1,12 @@
+using cfg;
+using UnityGameFramework.Runtime;
+
+namespace LFramework
+{
+    public class LuBanComponent : GameFrameworkComponent
+    {
+
+        public Tables AllTable { get; private set; }
+        
+    }
+}
